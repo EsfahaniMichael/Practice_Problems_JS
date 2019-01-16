7d219fdab431a4fbc3a2e02473a209e79b59dbdc
Esfahani <esfahanimichael@gmail.com>
2019-01-16 13:22:14 -0800

    finished working on problem twenty

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,16 @@
 answer.js
+
+var arrayPractice = [5,3,2,5,1]
+
+
+function randomOrder(array){
+  var newArray = [];  
+  for(var i = array.length; i > 0; i-- ){
+    var randomPosition = Math.floor(Math.random() * array.length);
+    newArray.push(array[randomPosition]);
+    array.splice(randomPosition, 1);      
+  }
+  return newArray; 
+}
+
+console.log(randomOrder(arrayPractice));
